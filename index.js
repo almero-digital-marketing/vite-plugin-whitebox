c99ace67b75a5d22c481db3abf564b9c89d7e2aa
dimitarkolev <dk@almero.bg>
2023-02-20 16:21:05 +0000

    Added vendor chunks

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import MID from 'node-machine-id'
 import os from 'os'
+import path from 'path'
 
-const mikserPlugin = ({ version, domain, context, recaptcha }) => {
+const mikserPlugin = ({ version, domain, context, recaptcha, vendor = {} }) => {
     return {
         name: 'mikser',
         config(config, { command }) {
@@ -11,6 +12,26 @@ const mikserPlugin = ({ version, domain, context, recaptcha }) => {
                 WHITEBOX_DOMAIN: JSON.stringify(domain),
                 WHITEBOX_CONTEXT: JSON.stringify(context || MID.machineIdSync() + '_' + os.hostname() + '_' + os.userInfo().username),
                 WHITEBOX_NO_RECAPTCHA: JSON.stringify(command == 'serve' ? recaptcha : ''),
+            })
+
+            config.build ||= {}
+            config.build.rollupOptions ||= {}
+            config.build.rollupOptions.output ||= {}
+            Object.assign(config.build.rollupOptions.output, {                
+                manualChunks: id => {
+                    if (id.includes('node_modules')) {
+                        let moduleName = id.split(path.sep).slice(id.split(path.sep).indexOf('node_modules') + 1)[0]
+                        for(let key in vendor) {
+                            if (vendor[key].indexOf(moduleName) > -1) return 'vendor-' + key
+                        }
+                        if (moduleName.includes('whitebox')) {
+                            return 'vendor-whitebox';
+                        } else if (moduleName.includes('vue')) {
+                            return 'vendor-vue';
+                        }
+                        return 'vendor';
+                    }
+                }
             })
         },
         configureServer(server) {
